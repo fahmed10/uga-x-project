f9d1a26b155a0697925e5f0e5733c1ad62faafaa
Samir Hernandez-Mendoza <smendoza5@ggc.edu>
2025-02-08 11:11:53 -0500

    added a sprite

--- client/src/main/java/entities/Player.java
+++ client/src/main/java/entities/Player.java
@@ -1,18 +1,25 @@
 package entities;
 
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 
 public class Player extends Entity {
     private static final int SIZE = 20;
+    private Image avatar;
 
     public Player(double startX, double startY) {
         super(startX, startY);
+        avatar = new Image(getClass().getResourceAsStream("/sprites/wimyits.jpg"));
     }
 
     @Override
     public void draw(GraphicsContext gc) {
         gc.setFill(Color.BLUE);
         gc.fillOval(worldX, worldY, SIZE, SIZE);
+        if (avatar != null) {
+            gc.drawImage(avatar, worldX, worldY, SIZE, SIZE);
+        }
+        
     }
 }
