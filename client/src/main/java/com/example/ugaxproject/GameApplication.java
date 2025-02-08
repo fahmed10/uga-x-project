10d433d606a45bab1055b17b6f13ff424887a5b0
Samir Hernandez-Mendoza <smendoza5@ggc.edu>
2025-02-07 23:08:24 -0500

    Added a simple Username box

--- client/src/main/java/com/example/ugaxproject/GameApplication.java
+++ client/src/main/java/com/example/ugaxproject/GameApplication.java
@@ -11,7 +11,7 @@ public class GameApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(GameApplication.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 500, 300);
         stage.setTitle("UGAHacks Game");
         stage.setScene(scene);
         stage.show();
