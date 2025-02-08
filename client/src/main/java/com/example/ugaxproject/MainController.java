95d09b052bb2db404fd6252293c33493809d8706
Samir Hernandez-Mendoza <smendoza5@ggc.edu>
2025-02-07 23:26:44 -0500

    added a button to the start screen

--- client/src/main/java/com/example/ugaxproject/MainController.java
+++ client/src/main/java/com/example/ugaxproject/MainController.java
@@ -1,5 +1,10 @@
 package com.example.ugaxproject;
 
+import javafx.event.ActionEvent;
+
 public class MainController {
 
+    public void handleStartGame(ActionEvent actionEvent) {
+        System.out.println("hello");
+    }
 }
