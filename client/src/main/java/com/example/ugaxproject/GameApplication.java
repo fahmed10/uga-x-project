fb831bc7cb488c5f13b76eb68a2a07b6afed8f35
Samir Hernandez-Mendoza <smendoza5@ggc.edu>
2025-02-07 23:44:42 -0500

    added a gamescreen

--- client/src/main/java/com/example/ugaxproject/GameApplication.java
+++ client/src/main/java/com/example/ugaxproject/GameApplication.java
@@ -4,17 +4,29 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class GameApplication extends Application {
+    private static Stage primaryStage;
+
     @Override
     public void start(Stage stage) throws IOException {
+        primaryStage = stage;
+        switchToStartScreen();
+        stage.setTitle("UGAHacks Game");
+        stage.show();
+    }
+
+    public static void switchToStartScreen() throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(GameApplication.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 500, 300);
-        stage.setTitle("UGAHacks Game");
-        stage.setScene(scene);
-        stage.show();
+        primaryStage.setScene(scene);
+    }
+
+    public static void switchToGameScreen() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(GameApplication.class.getResource("game-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        primaryStage.setScene(scene);
     }
 
     public static void main(String[] args) {
--- client/src/main/java/com/example/ugaxproject/MainController.java
+++ client/src/main/java/com/example/ugaxproject/MainController.java
@@ -2,9 +2,12 @@ package com.example.ugaxproject;
 
 import javafx.event.ActionEvent;
 
+import java.io.IOException;
+
 public class MainController {
 
-    public void handleStartGame(ActionEvent actionEvent) {
+    public void handleStartGame(ActionEvent actionEvent) throws IOException {
         System.out.println("hello");
+        GameApplication.switchToGameScreen();
     }
 }
